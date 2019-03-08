ae3e153d6a0157139308446daa22656773b635d7
poppabear8883 <servnx@gmail.com>
2019-03-08 14:44:43 -0500

    Initial Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <div v-for="server in servers" :key="server.id">
+      {{server.guild_id}}<br>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios';
+
+export default {
+  name: 'home',
+  components: {},
+  data() {
+    return {
+      servers: [],
+    };
+  },
+  methods: {
+    getServers() {
+      axios.get('http://viper.servnx.com:3050/api/servers')
+        .then(res => {
+          console.log(res);
+          this.servers = res.data;
+        })
+        .catch(err => console.log(err.message));
+    },
+  },
+  mounted() {
+    this.getServers();
+  },
+};
+</script>
