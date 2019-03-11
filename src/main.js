84995c1cb90ae470be4fe58db67353d3eebe1275
poppabear8883 <servnx@gmail.com>
2019-03-10 21:27:18 -0400

    Add Vuetify
    Add Icons
    Add VueAxios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
+import '@fortawesome/fontawesome-free/css/all.css';
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Vuetify from 'vuetify';
+
 import store from './store';
+import router from './router';
+
+import App from './App';
+import './App.scss';
+
+Vue.use(VueAxios, axios);
+
+Vue.use(Vuetify, {
+  theme: {
+    accent: '#0dc8df',
+  },
+  iconfont: 'fa',
+});
 
 Vue.config.productionTip = false;
 
