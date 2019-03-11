aac54d53be14d10be39a6be75f50325fdac0c677
poppabear8883 <servnx@gmail.com>
2019-03-10 21:27:49 -0400

    Update example test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import Statistics from '@/components/Statistics';
 
-describe('HelloWorld.vue', () => {
+describe('Statistics.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Statistics, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
